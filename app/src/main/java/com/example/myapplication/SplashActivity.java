901eb0c7f488d355783e3cc24456089c59571f50
Vinay0022 <vinu031995@gamil.com>
2022-06-30 19:57:48 +0530

    adding Login Page

---
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        make a fullscreen
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        //        start login activity after 2 sec
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+               finish();
+            }
+        } ,  2000);
+    }
+}
